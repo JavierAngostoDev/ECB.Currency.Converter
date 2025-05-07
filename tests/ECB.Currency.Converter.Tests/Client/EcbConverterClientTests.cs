2454d477704b73cf59318c2ad755ede940e3d8b1
Javier Angosto <javier.angosto@gmail.com>
2025-05-07 23:01:26 +0200

    add more tests

---
+++ tests/ECB.Currency.Converter.Tests/Client/EcbConverterClientTests.cs
@@ -0,0 +1,128 @@
+﻿using ECB.Currency.Converter.Client;
+using ECB.Currency.Converter.Client.Core.Common;
+using ECB.Currency.Converter.Client.Core.Domain;
+using ECB.Currency.Converter.Client.Core.Interfaces;
+using FluentAssertions;
+using Moq;
+
+namespace ECB.Currency.Converter.Tests.Client
+{
+    public class EcbConverterClientTests
+    {
+        private static readonly CurrencyEntity EUR = "EUR";
+        private static readonly DateTimeOffset Timestamp = DateTimeOffset.UtcNow;
+
+        [Fact]
+        public void Constructor_WithNullRateProvider_Should_Throw()
+        {
+            Action act = () => _ = new EcbConverterClient(null!);
+
+            act.Should().Throw<ArgumentNullException>()
+               .WithMessage("*rateProvider*");
+        }
+
+        [Fact]
+        public void Constructor_WithRateProvider_Should_Initialize()
+        {
+            IExchangeRateProvider provider = Mock.Of<IExchangeRateProvider>();
+            EcbConverterClient client = new EcbConverterClient(provider);
+
+            client.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task ConvertAsync_Should_Fail_When_FromCurrency_IsInvalid()
+        {
+            EcbConverterClient client = new EcbConverterClient(Mock.Of<IExchangeRateProvider>());
+
+            Result<MoneyEntity> result = await client.ConvertAsync("XX", "USD", 100m);
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be("Currency.Validation");
+        }
+
+        [Fact]
+        public async Task ConvertAsync_Should_Fail_When_ToCurrency_IsInvalid()
+        {
+            EcbConverterClient client = new EcbConverterClient(Mock.Of<IExchangeRateProvider>());
+
+            Result<MoneyEntity> result = await client.ConvertAsync("USD", "123", 100m);
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be("Currency.Validation");
+        }
+
+        [Fact]
+        public async Task ConvertAsync_Should_Succeed_When_AllInputsAreValid()
+        {
+            Mock<IExchangeRateProvider> providerMock = new Mock<IExchangeRateProvider>();
+            providerMock.Setup(p => p.GetLatestRatesAsync()).ReturnsAsync(
+                Result<IEnumerable<ExchangeRateEntity>>.Success(new[]
+                {
+                    ExchangeRateEntity.Create("EUR", "USD", 1.2m, Timestamp).Value,
+                    ExchangeRateEntity.Create("EUR", "EUR", 1.0m, Timestamp).Value
+                })
+            );
+
+            EcbConverterClient client = new EcbConverterClient(providerMock.Object);
+
+            Result<MoneyEntity> result = await client.ConvertAsync("USD", "EUR", 100m);
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Currency.Should().Be(EUR);
+        }
+
+        [Fact]
+        public void Dispose_Should_Not_Throw_When_Called_Multiple_Times()
+        {
+            EcbConverterClient client = new EcbConverterClient();
+
+            client.Dispose();
+            client.Dispose(); 
+        }
+
+        [Fact]
+        public async Task GetExchangeRateAsync_Should_Fail_When_FromCurrency_Invalid()
+        {
+            EcbConverterClient client = new EcbConverterClient(Mock.Of<IExchangeRateProvider>());
+
+            Result<ExchangeRateEntity> result = await client.GetExchangeRateAsync("!@#", "EUR");
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be("Currency.Validation");
+        }
+
+        [Fact]
+        public async Task GetExchangeRateAsync_Should_Succeed_When_Valid()
+        {
+            Mock<IExchangeRateProvider> providerMock = new Mock<IExchangeRateProvider>();
+            providerMock.Setup(p => p.GetLatestRatesAsync()).ReturnsAsync(
+                Result<IEnumerable<ExchangeRateEntity>>.Success(new[]
+                {
+                    ExchangeRateEntity.Create("EUR", "USD", 1.1m, Timestamp).Value,
+                    ExchangeRateEntity.Create("EUR", "EUR", 1.0m, Timestamp).Value
+                })
+            );
+
+            EcbConverterClient client = new EcbConverterClient(providerMock.Object);
+
+            Result<ExchangeRateEntity> result = await client.GetExchangeRateAsync("USD", "EUR");
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Rate.Should().BeApproximately(1.0m / 1.1m, 0.0001m);
+        }
+
+        [Fact]
+        public void GetLastRateUpdateTimestamp_Should_Delegate_ToProvider()
+        {
+            DateTimeOffset expected = DateTimeOffset.UtcNow.AddMinutes(-30);
+            Mock<IExchangeRateProvider> mock = new Mock<IExchangeRateProvider>();
+            mock.Setup(p => p.GetLastUpdateTimestamp()).Returns(expected);
+
+            EcbConverterClient client = new EcbConverterClient(mock.Object);
+            DateTimeOffset? result = client.GetLastRateUpdateTimestamp();
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ tests/ECB.Currency.Converter.Tests/Providers/EcbRateProviderTests.cs
@@ -0,0 +1,201 @@
+﻿using System.Net;
+using System.Text;
+using ECB.Currency.Converter.Client.Core.Common;
+using ECB.Currency.Converter.Client.Core.Domain;
+using ECB.Currency.Converter.Client.Infrastructure.Providers;
+using FluentAssertions;
+using Moq;
+using Moq.Protected;
+
+namespace ECB.Currency.Converter.Tests.Infrastructure.Providers
+{
+    public static class EcbRateProviderTestHelper
+    {
+        public static void ResetCache()
+        {
+            typeof(EcbRateProvider)
+                .GetField("_cachedRates", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                ?.SetValue(null, null);
+
+            typeof(EcbRateProvider)
+                .GetField("_cacheTimestamp", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                ?.SetValue(null, DateTimeOffset.MinValue);
+        }
+    }
+
+    public class EcbRateProviderTests
+    {
+        [Fact]
+        public async Task GetLastUpdateTimestamp_Should_Reflect_Cache()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            string xml = """
+            <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01"
+                             xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+              <Cube>
+                <Cube time="2024-05-07">
+                  <Cube Currency="USD" rate="1.00" />
+                </Cube>
+              </Cube>
+            </gesmes:Envelope>
+            """;
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.OK, xml);
+            EcbRateProvider provider = new EcbRateProvider(client, TimeSpan.FromHours(1));
+
+            DateTimeOffset? before = provider.GetLastUpdateTimestamp();
+            before.Should().BeNull();
+
+            await provider.GetLatestRatesAsync();
+
+            DateTimeOffset? after = provider.GetLastUpdateTimestamp();
+            after.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetLatestRatesAsync_Should_Return_Error_When_Currency_Is_Invalid()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            string xml = """
+            <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01"
+                             xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+              <Cube>
+                <Cube time="2024-05-07">
+                  <Cube Currency="INVALID" rate="1.2" />
+                </Cube>
+              </Cube>
+            </gesmes:Envelope>
+            """;
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.OK, xml);
+            EcbRateProvider provider = new EcbRateProvider(client);
+
+            Result<IEnumerable<ExchangeRateEntity>> result = await provider.GetLatestRatesAsync();
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be(EcbRateProvider.CurrencyParsingError.Code);
+        }
+
+        [Fact]
+        public async Task GetLatestRatesAsync_Should_Return_Error_When_Http_Fails()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.InternalServerError, "error");
+            EcbRateProvider provider = new EcbRateProvider(client);
+
+            Result<IEnumerable<ExchangeRateEntity>> result = await provider.GetLatestRatesAsync();
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be(EcbRateProvider.HttpError.Code);
+        }
+
+        [Fact]
+        public async Task GetLatestRatesAsync_Should_Return_Error_When_Rate_Is_Invalid()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            string xml = """
+            <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01"
+                             xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+              <Cube>
+                <Cube time="2024-05-07">
+                  <Cube Currency="USD" rate="INVALID" />
+                </Cube>
+              </Cube>
+            </gesmes:Envelope>
+            """;
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.OK, xml);
+            EcbRateProvider provider = new EcbRateProvider(client);
+
+            Result<IEnumerable<ExchangeRateEntity>> result = await provider.GetLatestRatesAsync();
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be(EcbRateProvider.RateParsingError.Code);
+        }
+
+        [Fact]
+        public async Task GetLatestRatesAsync_Should_Return_Error_When_Rates_Are_Empty()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            string xml = """
+            <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01"
+                             xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+              <Cube>
+                <Cube time="2024-05-07">
+                </Cube>
+              </Cube>
+            </gesmes:Envelope>
+            """;
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.OK, xml);
+            EcbRateProvider provider = new EcbRateProvider(client);
+
+            Result<IEnumerable<ExchangeRateEntity>> result = await provider.GetLatestRatesAsync();
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be(EcbRateProvider.NoRatesFoundError.Code);
+        }
+
+        [Fact]
+        public async Task GetLatestRatesAsync_Should_Return_Error_When_Response_Is_Unparseable()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.OK, "<invalid><xml>");
+            EcbRateProvider provider = new EcbRateProvider(client);
+
+            Result<IEnumerable<ExchangeRateEntity>> result = await provider.GetLatestRatesAsync();
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Code.Should().Be(EcbRateProvider.XmlParsingError.Code);
+        }
+
+        [Fact]
+        public async Task GetLatestRatesAsync_Should_Return_Success_When_Valid_Xml_Provided()
+        {
+            EcbRateProviderTestHelper.ResetCache();
+
+            string xml = """
+            <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01"
+                             xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+              <Cube>
+                <Cube time="2024-05-07">
+                  <Cube Currency="USD" rate="1.12" />
+                </Cube>
+              </Cube>
+            </gesmes:Envelope>
+            """;
+
+            HttpClient client = CreateMockHttpClient(HttpStatusCode.OK, xml);
+            EcbRateProvider provider = new EcbRateProvider(client);
+
+            Result<IEnumerable<ExchangeRateEntity>> result = await provider.GetLatestRatesAsync();
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().ContainSingle();
+            result.Value.First().QuoteCurrency.Code.Should().Be("USD");
+            result.Value.First().Rate.Should().Be(1.12m);
+        }
+
+        private static HttpClient CreateMockHttpClient(HttpStatusCode statusCode, string content)
+        {
+            Mock<HttpMessageHandler> handler = new Mock<HttpMessageHandler>();
+            handler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = statusCode,
+                    Content = new StringContent(content, Encoding.UTF8, "application/xml")
+                });
+
+            return new HttpClient(handler.Object);
+        }
+    }
+}
