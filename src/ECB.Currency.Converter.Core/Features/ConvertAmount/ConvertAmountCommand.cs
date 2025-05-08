878b8858b3bff569d4704ccdeecfb2aa24da1727
Javier Angosto <javier.angosto@gmail.com>
2025-05-08 19:59:29 +0200

    delete deprecated projects

--- src/ECB.Currency.Converter.Core/Features/ConvertAmount/ConvertAmountCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using ECB.Currency.Converter.Core.Domain;
-
-namespace ECB.Currency.Converter.Core.Features.ConvertAmount;
-
-internal record ConvertAmountCommand(MoneyEntity SourceMoney, CurrencyEntity TargetCurrency);
--- src/ECB.Currency.Converter.Core/Features/GetExchangeRate/GetExchangeRateQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using ECB.Currency.Converter.Core.Domain;
-
-namespace ECB.Currency.Converter.Core.Features.GetExchangeRate;
-
-internal record GetExchangeRateQuery(CurrencyEntity FromCurrency, CurrencyEntity ToCurrency);
